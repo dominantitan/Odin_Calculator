b3b14c9a0dd138b21ea680ed6d8d57eabb051019
dominantitan <kingaadit3@gmail.com>
2026-02-11 23:36:12 +0545

    Add Broken logic for calculator bored af will do tommorrow

--- script.js
+++ script.js
@@ -1,42 +1,78 @@
-let number1 = 0;
-let number2 = 0;
+let number1;
+let number2;
+let tempNum1;
 let operator = "";
+let numbers = "1234567890";
+let operators = "+-*/.";
+let evaluate = "=";
 
-function operate(num1,num2,op){
-    let value;
-    switch(op){
-        case "+":
-            value = add(num1,num2);
-            break;
-        case "-":
-            value = sub(num1,num2);
-            break;
-        case "*":
-            value = multiply(num1,num2);
-            break;
-        case "/":
-            value = divide(num1,num2);
-            break;
-        default:
-            value = 0;
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    if (number1 === undefined) {
+      if (numbers.includes(event.target.id)) {
+        number1 = +event.target.id;
+        console.log(number1);
+      }else if(operators.includes(event.target.id)){
+        operator = event.target.id;
+        console.log(operator);
+      }else if(event.target.id = evaluate){
+        number1 = operate(tempNum1,number2,operator);
+        console.log(number1);
+      }
+    }else {
+        if (numbers.includes(event.target.id)) {
+        number2 = +event.target.id;
+        tempNum1 = number1;
+        number1 = undefined;
+        console.log(number2);
+      }else if(operators.includes(event.target.id)){
+        operator = event.target.id;
+        console.log(operator);
+      }else if(event.target.id === evaluate){
+        number1 = operate(tempNum1,number2,operator);
+        console.log(number1);
+      }
     }
+  });
+});
 
-    return value;
+
+function operate(num1, num2, op) {
+  let value;
+  switch (op) {
+    case "+":
+      value = add(num1, num2);
+      break;
+    case "-":
+      value = sub(num1, num2);
+      break;
+    case "*":
+      value = multiply(num1, num2);
+      break;
+    case "/":
+      value = divide(num1, num2);
+      break;
+    default:
+      value = 0;
+  }
+
+  return value;
 }
 
-function add(a,b){
-    return a + b;
+function add(a, b) {
+  return a + b;
 }
 
-
-function sub(a,b){
-    return a - b;
+function sub(a, b) {
+  return a - b;
 }
 
-function multiply(a,b){
-    return a * b;
+function multiply(a, b) {
+  return a * b;
 }
 
-function divide(a,b){
-    return a/b;
+function divide(a, b) {
+  return a / b;
 }
