ec5801ea752eff14b781ed4283738f5984a40ab5
dominantitan <kingaadit3@gmail.com>
2026-02-12 15:37:17 +0545

    Add some shitty code

--- script.js
+++ script.js
@@ -1,42 +1,104 @@
-let number1;
-let number2;
+let number1 = "";
+let number2 = "";
 let tempNum1;
 let operator = "";
 let numbers = "1234567890";
-let operators = "+-*/.";
+let operators = "+-*/";
+let operatorAppeared = false;
+let isStored = false;
 let evaluate = "=";
 
-const buttons = document.querySelectorAll("button");
+const primaryDisplay = document.querySelector("#displayText");
+const secDisplay = document.querySelector("#secondaryDisplay");
 
-buttons.forEach((button) => {
-  button.addEventListener("click", (event) => {
-    if (number1 === undefined) {
-      if (numbers.includes(event.target.id)) {
-        number1 = +event.target.id;
-        console.log(number1);
-      }else if(operators.includes(event.target.id)){
-        operator = event.target.id;
-        console.log(operator);
-      }else if(event.target.id = evaluate){
-        number1 = operate(tempNum1,number2,operator);
-        console.log(number1);
-      }
-    }else {
-        if (numbers.includes(event.target.id)) {
-        number2 = +event.target.id;
-        tempNum1 = number1;
-        number1 = undefined;
-        console.log(number2);
-      }else if(operators.includes(event.target.id)){
-        operator = event.target.id;
-        console.log(operator);
-      }else if(event.target.id === evaluate){
-        number1 = operate(tempNum1,number2,operator);
-        console.log(number1);
-      }
-    }
-  });
-});
+// const buttons = document.querySelectorAll("button");
+
+// buttons.forEach((button) => {
+//   button.addEventListener("click", (event) => {
+//     if (number1 === undefined) {
+//       if (numbers.includes(event.target.id)) {
+//         number1 = +event.target.id;
+//         console.log(number1);
+//       }else if(operators.includes(event.target.id)){
+//         operator = event.target.id;
+//         console.log(operator);
+//       }else if(event.target.id = evaluate){
+//         number1 = operate(tempNum1,number2,operator);
+//         console.log(number1);
+//       }
+//     }else {
+//         if (numbers.includes(event.target.id)) {
+//         number2 = +event.target.id;
+//         tempNum1 = number1;
+//         number1 = undefined;
+//         console.log(number2);
+//       }else if(operators.includes(event.target.id)){
+//         operator = event.target.id;
+//         console.log(operator);
+//       }else if(event.target.id === evaluate){
+//         number1 = operate(tempNum1,number2,operator);
+//         console.log(number1);
+//       }
+//     }
+//   });
+// });
+
+
+
+function buttonClicked(){
+    const buttons = document.querySelectorAll("button");
+
+    buttons.forEach((button) =>{
+      button.addEventListener("click",(event) =>{
+        if(isNumber(event.target.id) && operatorAppeared === false){
+          number1 += event.target.id;
+          console.log(number1);
+          overwriteDisplay(number1);
+        }else if(isOperator(event.target.id) && operatorAppeared === false){
+          operator = event.target.id;
+          updateDisplay(operator);
+          console.log(operator);
+          operatorAppeared = true;
+        }else if(isNumber(event.target.id) && operatorAppeared === true){
+          number2 += event.target.id;
+          if(isStored === false){
+          const prevExpression = primaryDisplay.textContent;
+          secDisplay.textContent = prevExpression;
+          isStored = true;
+          }
+          overwriteDisplay(number2);
+          console.log(number2);
+        }else if(isCharEquals(event.target.id) && operatorAppeared === true){
+          number1 = +number1;
+          number2 = +number2;
+          let result = operate(number1,number2,operator);
+          overwriteDisplay(result);
+          console.log(result);
+          operatorAppeared = false;
+          isStore = false;
+        }
+      })
+    })
+}
+
+function isNumber(char){
+  return numbers.includes(char);
+}
+
+function isOperator(char){
+  return operators.includes(char);
+}
+
+function isCharEquals(char){
+  return char === "=";
+}
+
+function updateDisplay(text){
+      primaryDisplay.textContent += text;
+}
+function overwriteDisplay(text){
+      primaryDisplay.textContent = text;
+}
 
 
 function operate(num1, num2, op) {
@@ -76,3 +138,5 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+buttonClicked();
